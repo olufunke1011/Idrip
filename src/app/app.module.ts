82082f98255e4a24421e4bee78cdb8d972ea6e4b
olufunke1011 <owoseniolufunke1011@gmail.com>
2020-12-16 14:48:15 +0100

    creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { PoliticsComponent } from './politics/politics.component';
+import { HealthComponent } from './health/health.component';
+import { EntertainmentComponent } from './entertainment/entertainment.component';
+import { SportsComponent } from './sports/sports.component';
+import { BusinessComponent } from './business/business.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NewsComponent,
+    PoliticsComponent,
+    HealthComponent,
+    EntertainmentComponent,
+    SportsComponent,
+    BusinessComponent
   ],
   imports: [
     BrowserModule,
