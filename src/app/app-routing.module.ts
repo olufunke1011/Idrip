a6f9533937ae528b18bea6466bc5f87aa36763b7
olufunke1011 <owoseniolufunke1011@gmail.com>
2020-12-16 14:10:30 +0100

    modify app routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { homeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: homeComponent },
+  { path: '', component: HomeComponent },
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' },
