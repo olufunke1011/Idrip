fb1645804a324161b3ab588be165b42c09697144
olufunke1011 <owoseniolufunke1011@gmail.com>
2020-12-16 14:04:48 +0100

    create route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { homeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: homeComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
